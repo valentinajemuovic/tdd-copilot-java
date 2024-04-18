b8f22418933194275535b89a3a4b1d0a7f3a0dae
Valentina Cupać <valentina.cupac@optivem.com>
2024-04-18 15:06:54 +0200

    Initial commit

---
+++ src/main/java/com/optivem/tddaijava/TddAiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.optivem.tddaijava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddAiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddAiJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/optivem/tddaijava/TddAiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optivem.tddaijava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddAiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
